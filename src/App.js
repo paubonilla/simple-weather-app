a1b7710ba1db4a814586c1ed0a204bbeaf79cefb
Pau Bonilla <paulo111995@gmail.com>
2020-08-05 01:04:34 -0700

    modified styles; made it darkmode

--- src/App.js
+++ src/App.js
@@ -20,20 +20,15 @@ export const SearchBox = styled.div`
     display: block;
     width: 100%;
     padding: 15px;
-
     appearance: none;
     background: none;
     border: none;
     outline: none;
-
     background-color: rgba(255, 255, 255, 0.5);
     border-radius: 5px;
-
     box-shadow: 0px 5px rgba(0, 0, 0, 0.2);
-
-    color: #313131;
+    color: #333436;
     font-size: 20px;
-
     transition: 0.4s ease;
 
     &:focus {
