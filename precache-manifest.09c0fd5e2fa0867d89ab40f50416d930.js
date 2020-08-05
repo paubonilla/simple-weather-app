604dd1e935098a72bd3c79fcfa3b9164cafb3dd8
Pau Bonilla <paulo111995@gmail.com>
2020-08-05 00:58:30 -0700

    Updates

---
+++ precache-manifest.09c0fd5e2fa0867d89ab40f50416d930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec954563aad1615c8b1c9ab450123cc6",
+    "url": "/simple-weather-app/index.html"
+  },
+  {
+    "revision": "616b25c5decf35cf082a",
+    "url": "/simple-weather-app/static/css/main.c20247d7.chunk.css"
+  },
+  {
+    "revision": "8e5757aa737593223a99",
+    "url": "/simple-weather-app/static/js/2.1ebf5ac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-weather-app/static/js/2.1ebf5ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616b25c5decf35cf082a",
+    "url": "/simple-weather-app/static/js/main.3a45037b.chunk.js"
+  },
+  {
+    "revision": "50d19f41bf32d9ef1ecc",
+    "url": "/simple-weather-app/static/js/runtime-main.4278c608.js"
+  }
+]);
